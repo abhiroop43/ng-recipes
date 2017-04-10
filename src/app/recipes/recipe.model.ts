e501358a6590e4322aaad03b756377630863426c
Abhiroop Santra <abhiroop.santra@gmail.com>
2017-04-10 15:11:21 +0400

    added components

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,11 @@
-/**
- * Created by asantra on 4/10/2017.
- */
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
